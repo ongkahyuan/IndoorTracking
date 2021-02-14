d0fbe534f73f9e71a07653b027c3154a3ba7420d
sazzyboy <43925277+sazzyboy@users.noreply.github.com>
2021-02-14 16:02:22 +0800

    added buttons for mapping and viewing saved locations in map tab

---
+++ app/src/main/java/com/example/indoortracking/tracking/map/BeginMapping.java
@@ -0,0 +1,18 @@
+package com.example.indoortracking.tracking.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.indoortracking.R;
+
+public class BeginMapping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin_mapping);
+
+
+    }
+}
--- app/src/main/java/com/example/indoortracking/tracking/map/map.java
+++ app/src/main/java/com/example/indoortracking/tracking/map/map.java
@@ -1,5 +1,6 @@
 package com.example.indoortracking.tracking.map;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,16 +9,48 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Display;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.indoortracking.R;
 
-public class map extends AppCompatActivity {
+public class map extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
+        Button mapNewLocation = findViewById(R.id.mapnewlocation);
+        Button viewSavedLocation = findViewById(R.id.viewsavedlocations);
+
+        mapNewLocation.setOnClickListener(this);
+        viewSavedLocation.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mapnewlocation:
+                NewLocation();
+            case R.id.viewsavedlocations:
+                viewLocation();
+        }
+
+
+    }
+
+    private void viewLocation() {
+        Intent intent2 = new Intent(this, DisplayMap.class);
+        startActivity(intent2);
+    }
+
+    private void NewLocation() {
+        Intent intent1 = new Intent(this, BeginMapping.class);
+        startActivity(intent1);
     }
 }
